58cfb38edc486acfde794a6b7edf0c966a0ce6dd
luoxiang723 <515088980@qq.com>
2018-02-05 14:27:13 +0800

    首次提交

---
+++ src/main/java/com/lx/example/controller/WebController.java
@@ -0,0 +1,21 @@
+package com.lx.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/web")
+public class WebController {
+
+	@RequestMapping(value = "index")
+	public String index(ModelMap map) {
+		map.put("title", "freemarker hello word");
+		return "index"; // 开头不要加上/，linux下面会出错
+	}
+	
+	@RequestMapping(value = "error")
+	public String error(ModelMap map) {
+		throw new RuntimeException("测试异常");
+	}
+}
